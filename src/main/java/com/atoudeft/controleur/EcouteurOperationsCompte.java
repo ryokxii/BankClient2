efbc0e6fd214b38d06b475efa678ef62b7554b66
Zafer A <lssjshisui@gmail.com>
2024-12-04 11:43:36 -0500

    2.1 fini

--- src/main/java/com/atoudeft/controleur/EcouteurOperationsCompte.java
+++ src/main/java/com/atoudeft/controleur/EcouteurOperationsCompte.java
@@ -5,6 +5,8 @@ import com.atoudeft.client.Client;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
 public class EcouteurOperationsCompte implements ActionListener {
     private Client client;
 
@@ -14,7 +16,14 @@ public class EcouteurOperationsCompte implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        //à compléter :
-        
+        // à compléter :
+        Object source = e.getSource();
+        String nomAction;
+        if (source instanceof JButton) {
+            nomAction = ((JButton) source).getActionCommand();
+            if (nomAction.equals("EPARGNE")) {
+                client.envoyer("EPARGNE");
+            }
+        }
     }
 }
